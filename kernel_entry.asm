f2c4beca8357687e9dd1f5393fcf826253bbf2dc
NewtonJoaquim <newtonjoaquim12@gmail.com>
2025-11-16 17:56:58 -0300

    simplify kernel entry

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -11,12 +11,6 @@ _start:
     ; Set up the stack
     mov esp, 0x9000
     
-    ; Test: Write directly to VGA to verify kernel is executing
-    mov edi, 0xB8000
-    mov word [edi], 0x0F41      ; White 'A' on black
-    mov word [edi+2], 0x0E42    ; Yellow 'B' on black
-    mov word [edi+4], 0x0D43    ; Magenta 'C' on black
-    
     ; Call the C main function
     call main
     
