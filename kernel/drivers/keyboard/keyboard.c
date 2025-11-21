74cfcb7eb7863ed98f67f68d651f33410a07e173
NewtonJoaquim <newtonjoaquim12@gmail.com>
2025-11-21 16:48:13 -0300

    add shift logic to keyboard

--- kernel/drivers/keyboard/keyboard.c
+++ kernel/drivers/keyboard/keyboard.c
@@ -1,8 +1,23 @@
 #include "keyboard.h"
 
+static int shift_pressed = 0;
+
 void keyboard_handler(void) {
     uint8_t scancode = inb(0x60);   // read from keyboard port
     
+    // Handle Shift press/release
+    if (scancode == 0x2A || scancode == 0x36) {
+        shift_pressed = 1;
+        outb(0x20, 0x20);
+        return;
+    }
+    if (scancode == 0xAA || scancode == 0xB6) {
+        shift_pressed = 0;
+        outb(0x20, 0x20);
+        return;
+    }
+
+
     // Ignore key releases
     if (scancode & 0x80) {
         outb(0x20, 0x20); // EOI
@@ -10,7 +25,13 @@ void keyboard_handler(void) {
     }
 
     char c = translate_scancode(scancode); // simple lookup
-    vga_put_char(c);
+     if (c) {
+        if (shift_pressed && c >= 'a' && c <= 'z') {
+            c = c - 32; // convert to uppercase ASCII
+        }
+        vga_put_char(c);
+    }
+
     
     outb(0x20, 0x20);               // send EOI to PIC
 }
