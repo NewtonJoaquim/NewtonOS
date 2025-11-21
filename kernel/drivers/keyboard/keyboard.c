e8b93f2f8306b22bd7ee89276c9528b6868dba7b
NewtonJoaquim <newtonjoaquim12@gmail.com>
2025-11-21 17:01:49 -0300

    add capslock logic

--- kernel/drivers/keyboard/keyboard.c
+++ kernel/drivers/keyboard/keyboard.c
@@ -1,45 +1,62 @@
 #include "keyboard.h"
 
 static int shift_pressed = 0;
+static int capslock_pressed = 0;
 
-void keyboard_handler(void) {
-    uint8_t scancode = inb(0x60);   // read from keyboard port
-    
+void keyboard_handler(void)
+{
+    uint8_t scancode = inb(0x60); // read from keyboard port
+
     // Handle Shift press/release
-    if (scancode == 0x2A || scancode == 0x36) {
+    if (scancode == 0x2A || scancode == 0x36)
+    {
         shift_pressed = 1;
         outb(0x20, 0x20);
         return;
     }
-    if (scancode == 0xAA || scancode == 0xB6) {
+    if (scancode == 0xAA || scancode == 0xB6)
+    {
         shift_pressed = 0;
         outb(0x20, 0x20);
         return;
     }
 
+    // Handle capslock press
+    if (scancode == 0x34)
+    {
+        capslock_pressed = !capslock_pressed;
+        outb(0x20, 0x20);
+        return;
+    }
 
     // Ignore key releases
-    if (scancode & 0x80) {
+    if (scancode & 0x80)
+    {
         outb(0x20, 0x20); // EOI
         return;
     }
 
     char c = translate_scancode(scancode); // simple lookup
-     if (c) {
+    if (c)
+    {
         vga_put_char(c);
     }
 
-    
-    outb(0x20, 0x20);               // send EOI to PIC
+    outb(0x20, 0x20); // send EOI to PIC
 }
 
-char translate_scancode(uint8_t scancode) {
-    if (scancode < 128) {
-        if (shift_pressed)
+char translate_scancode(uint8_t scancode)
+{
+    if (scancode < 128)
+    {
+        if (shift_pressed && capslock_pressed)
+            return scancode_table_shift_caps[scancode];
+        else if (shift_pressed)
             return scancode_table_shift[scancode];
+        else if (capslock_pressed)
+            return scancode_table_caps[scancode];
         else
             return scancode_table[scancode];
     }
     return 0;
-
 }
